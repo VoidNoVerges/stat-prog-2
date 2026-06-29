0855d33d4f0181ee1044113c6599b84dcddcd4df
VoidNoVerges <241015669+VoidNoVerges@users.noreply.github.com>
2026-06-29 19:06:14 +0200

    Add comment about what columns to add or remove

--- code/02_clean.R
+++ code/02_clean.R
@@ -6,6 +6,53 @@ library(here)
 
 raw <- read_csv(here("data", "raw", "ai_student_impact_dataset (1).csv"))
 
+"
+Which columns to add/remove?
+
+
+question 1: In which majors is the GPA most affected by heavier AI Usage?
+The goal is to show in which majors which amount of AI usage is helps the GPA
+in general, not how AI should be used (e.g. with Tool_Diversity). For that is
+question 2.
+
+Heaviness of AI Usage: This is best decribed by
+\"Weekly_GenAI_Hours / Traditional_Study_Hours + Weekly_GenAI_Hours\"
+as it measures to which percentage AI used to learn.
+Note that this does not measure how much the students learned in general.
+
+Effect: The effect can be 1. the GPA change over time (Pre_Semester_GPA vs
+Post_Semester_GPA) or 2. how good the GPA of the students is in general
+when using AI using the average of the Pre_Semester_GPA and the
+Post_Semester_GPA.
+But 2. is a pretty wonky indicator as it has many biases (e.g. students having a
+different average GPA depending on the major). So we only use 1. as it actually
+shows us the effects over time.
+
+=> Add 2 new columns GPA_Change_Over_Semester and Percentage_Of_AI_Usage
+
+
+question 2: How can AI be used for the best GPA score?
+
+Another dimension to add is the Weekly_Study_Hours.
+As we already have GPA_Change_Over_Semester the differentiation between
+Traditional_Study_Hours and Weekly_GenAI_Hours is redundant and it is better
+saved in Weekly_Study_Hours (Traditional_Study_Hours + Weekly_GenAI_Hours).
+So the model tells you how much to learn and to which percentage use AI
+which is easier to interprete than just Traditional_Study_Hours vs
+Weekly_GenAI_Hours.
+Traditional_Study_Hours and Weekly_GenAI_Hours should now be removed as they
+just describe Percentage_Of_AI_Usage and Weekly_Study_Hours.
+
+=> Add 1 new column Weekly_Study_Hours and remove 2 columns
+   Traditional_Study_Hours and Weekly_GenAI_Hours
+
+
+=> TO ADD: GPA_Change_Over_Semester, Percentage_Of_AI_Usage, Weekly_Study_Hours.
+   TO REMOVE: Traditional_Study_Hours, Weekly_GenAI_Hours
+"
+
+
+
 cleaned <- raw |>
   # Ensure no duplicate student IDs.
   dplyr::distinct() |>
